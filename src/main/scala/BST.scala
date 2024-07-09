2d74640bc4c6651ce0639889345b4ee3d3052afc
Dave Shah 6480253 <145308523+Mynameisdaves@users.noreply.github.com>
2024-07-09 14:25:49 +0000

    Attempted to implement insert into the function using parallelization techniques

--- src/main/scala/BST.scala
+++ src/main/scala/BST.scala
@@ -1,3 +1,8 @@
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.collection.parallel.CollectionConverters._
+
 object BST {
 
   trait BST[+T]
@@ -12,15 +17,33 @@ object BST {
     // auxiliary constructors
     def this(k: T) = this(k, Empty, Empty)
     def this(k: T, l: BST[T]) = this(k, l, Empty)
-
   }
 
-  case class ParBST[T] (r: BST[T]) extends BST[T] {
-    var root: BST[T] = r
+  class ParBST[T](initialRoot: BST[T] = Empty)(implicit ord: Ordering[T]) {
+    @volatile var root: BST[T] = initialRoot
 
-    // auxiliary constructors
-    def this() = this(Empty)
+    private def inserthelpfunction(node: BST[T], key: T): BST[T] = node match {
+      case Empty => Node(key)
+      case Node(k, l, r) =>
+        if (ord.lt(key, k)) {
+          Node(k, insertHelper(l, key), r)
+        } else if (ord.gt(key, k)) {
+          Node(k, l, insertHelper(r, key))
+        } else {
+          node 
+        }
+    }
 
+    def insert(keys: Seq[T]): Future[Unit] = {
+      val futures = keys.par.map { key =>
+        Future {
+          this.synchronized {
+            root = inserthelpfunction(root, key)
+          }
+        }
+      }.toList
+
+      Future.sequence(futures).map(_ => ())
+    }
   }
-
 }
